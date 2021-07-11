dabd0ff136074a3d0a6e73fda55c6a40229befbf
faithsila <6210110586@psu.ac.th>
2021-07-11 22:14:41 +0700

    part7-4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import WordCard from './WordCard';
 import './App.css';
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '' };
+  }
+  myChangeHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
+  render() {
+    return (
+      <form>
+      <h1>Hello {this.state.username}</h1>
+      <p>Enter your name:</p>
+      <input
+        type='text'
+        onChange={this.myChangeHandler}
+      />
+      </form>
+    );
+  }
+}
 
+ReactDOM.render(<MyForm />, document.getElementById('root'));
 const word = "Hello";
 function App() {
   return (
