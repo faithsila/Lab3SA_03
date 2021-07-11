2a377460c0eb8a1709cfa1634a5c957f96db097b
faithsila <6210110586@psu.ac.th>
2021-07-11 21:41:59 +0700

    part7-2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ const word = "Hello";
 function App() {
   return (
     <div>
-    {
+      <p>Enter your name:</p>
+      <input
+       type="text"
+       />
+    {   
     <WordCard value = {word}/>
     }
     </div>
